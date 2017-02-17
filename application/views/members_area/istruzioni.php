72553b9558efaf1860b3a8c0164a5654f2a0a6bb
Chiara <axeda@libero.it>
2017-02-17 14:06:39 +0100

    Aggiunta pagina con le istruzioni

---
+++ application/views/members_area/istruzioni.php
@@ -0,0 +1,65 @@
+<div class="right_col" role="main">
+	<div class="">
+		<div class="row">
+  		<div class="page-title">
+  			<div class ="col-md-12">
+    				<div class="title_left">
+      				<h3>Istruzioni per l'uso dell'applicazione</h3>        				
+    				</div>
+    			</div>
+  		</div>
+    </div>
+  	<div class="row">
+      <div class="col-md-6 col-xs-12">
+        <div class="x_panel">
+          <div class="x_content">
+            <div class="" role="tabpanel" data-example-id="togglable-tabs">
+              <ul id="myTab" class="nav nav-tabs bar_tabs" role="tablist">
+                <li role="presentation" class="active">
+                  <a href="#tab_content1" id="meteo-tab" role="tab" data-toggle="tab" aria-expanded="true">Meteorologi</a>
+                </li>
+                <li role="presentation" class="">
+                  <a href="#tab_content2" role="tab" id="dirigente-tab" data-toggle="tab" aria-expanded="false">Dirigenti</a>
+                </li>
+              </ul>
+              <div id="myTabContent" class="tab-content">
+                <div role="tabpanel" class="tab-pane fade active in" id="tab_content1" aria-labelledby="meteo-tab">
+                  <p>
+                      I previsori devono compilare le previsioni (<i>Le tue previsioni</i>) ogni giorno per le seguenti fasce orarie: 
+                      <ul>
+                        <li>12-18 del giorno stesso</li>
+                        <li>18-24 del giorno stesso</li> 
+                        <li>0-6 del giorno successivo</li> 
+                        <li>6-12 del giorno successivo</li> 
+                        <li>12-18 del giorno successivo</li> 
+                        <li>18-24 del giorno successivo </li>     
+                      </ul>      
+                      Per ogni zona saranno disponibili le diverse fasce orarie, e per ogni fascia oraria la previsione potrà essere "<b>Nessuna precipitazione</b> (allerta verde)", "<b>FS</b> (allerta gialla)"o "<b>FD</b> (allerta arancione)". 
+                      Una volta fatta la compilazione, andando avanti i dati saranno riepilogati e sarà possibile confermarli o tornare indietro per modificarli.  </br>
+                      Dopo le ore 12:00, le prime due fasce orarie non saranno più disponibili. </br>
+                      Dopo la compilazione delle previsioni sarà possibile anche visualizzare, fare ricerche e salvare i dati delle previsioni di tutti gli utenti, in forma anonima (<i>Confronto previsioni</i>). </br>
+                      La ricerca (per data, allerta, o zona) all'interno delle previsioni fatte dall'utente loggato in quel momento è sempre disponibile (<i>Archivio previsioni</i>).</br>
+                      I risultati delle ricerche possono sempre essere salvati nei formati csv o excel, oppure copiati o stampati.
+                      Ogni utente può visualizzare i dati relativi al proprio profilo (<i>Il tuo profilo</i>) ed eventualmente modificare la password di accesso.
+                  </p>
+                </div>
+                <div role="tabpanel" class="tab-pane fade" id="tab_content2" aria-labelledby="dirigente-tab">
+                  <p>
+                    I dirigenti, oltre a poter visualizzare il proprio profilo (<i>Il tuo profilo</i>) ed eventualmente modificare la password di accesso, possono fare ricerche all'interno dell'archivio delle previsioni (<i>Archivio previsioni</i>), filtrando per:
+                        <ul>
+                          <li>Data o range di date</li>
+                          <li>Una o più zone (o tutte)</li> 
+                          <li>Una o più allerte (o tutte)</li> 
+                          <li>Uno o più meteorologi (o tutti)</li> 
+                        </ul>    
+                    I risultati delle ricerche possono sempre essere salvati nei formati csv o excel, oppure copiati o stampati. 
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
