17fbd8d71b564791c432aaf34c16e4450feebd12
axeda81 <axeda@libero.it>
2017-05-16 18:03:47 +0200

    due view aggiunte per la parte di inserimento dati storici

---
+++ application/views/members_area/meteo/archivio.php
@@ -0,0 +1,94 @@
+<!-- page content -->
+<div class="right_col" role="main">
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Archivio previsioni</h3>
+      </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="row">
+      <div class="col-md-3 col-xs-12 widget widget_tally_box">
+        <div class="x_panel">
+          <div class="x_content">
+            <div class="flex">
+              <ul class="list-inline widget_profile_box text-center">
+                <li class="text-center" style="float: none;">
+                  <a style="float: none;" id="rivediPrevisioni">
+                    <i class="fa fa-sun-o"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <h3 class="name">Archivio tue previsioni</h3>
+            <p>
+              Rivedi tutte le previsioni che hai fatto.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-xs-12 widget widget_tally_box">
+        <div class="x_panel">
+          <div class="x_content">
+            <div class="flex">
+              <ul class="list-inline widget_profile_box text-center">
+                <li class="text-center" style="float: none;">
+                  <a style="float: none;" id="previsioniTutti">
+                    <i class="fa fa-archive"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <h3 class="name">Confronto previsioni</h3>
+            <p>
+              Vedi le previsioni di tutti gli utenti, in forma anonima.
+            </p>
+          </div>
+        </div>
+      </div>      
+      <div class="col-md-3 col-xs-12 widget widget_tally_box">
+        <div class="x_panel">
+          <div class="x_content">
+            <div class="flex">
+              <ul class="list-inline widget_profile_box text-center">
+                <li class="text-center" style="float: none;">
+                  <a style="float: none;" id="inserisciDati">
+                    <i class="fa fa-user"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <h3 class="name">Inserisci dati </h3>
+            <p>
+              Inserisci dati storici relativi a tue previsioni.
+            </p>
+          </div>
+        </div>
+      </div>    
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('#rivediPrevisioni').on('click', function() {
+      window.location.replace("<?php echo base_url();?>index.php/archivio/ricercaperutente");
+    })
+  });
+</script>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('#previsioniTutti').on('click', function() {
+      window.location.replace("<?php echo base_url();?>index.php/archivio/ricerca_tutti");
+    })
+  });
+</script>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('#inserisciDati').on('click', function() {
+      window.location.replace("<?php echo base_url();?>index.php/archivio/inserisciDati");
+    })
+  });
+</script>
